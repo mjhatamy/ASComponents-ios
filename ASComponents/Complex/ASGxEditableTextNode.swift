942d8807e3dfca3ae74be288f5631983b9bf19d7
Majid Hatami <mjhatamy@gmail.com>
2019-07-25 08:19:04 -0700

    some changes

--- ASComponents/Complex/ASGxEditableTextNode.swift
+++ ASComponents/Complex/ASGxEditableTextNode.swift
@@ -9,13 +9,21 @@
 import UIKit
 import AsyncDisplayKit
 
-public class ASGxEditableTextNode: ASTextNode {
+public protocol ASGxEditableTextNodeProtocol {
+    func editableTextNode(_ editableTextNode: ASEditableTextNode, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool
+}
+
+public class ASGxEditableTextNode: ASDisplayNode, ASEditableTextNodeDelegate {
+    public var delegate: ASGxEditableTextNodeProtocol?
+    /// Default is 0 or unlimited
+    public var maxTextLength: Int = 0;
     public var textViewInset: UIEdgeInsets = .zero
     public let textNode:ASEditableTextNode = ASEditableTextNode()
     public override init(){
         super.init()
         self.automaticallyManagesSubnodes = true
         self.isUserInteractionEnabled = true
+        self.textNode.delegate = self;
     }
     
     public override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
@@ -31,5 +39,17 @@ public class ASGxEditableTextNode: ASTextNode {
         guard let touchPoint = touches.first?.location(in: self.view) else { return }
         guard self.bounds.contains(touchPoint) else { return }
         self.textNode.becomeFirstResponder()
+    }
+    
+    
+    public func editableTextNode(_ editableTextNode: ASEditableTextNode, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if self.maxTextLength > 0 {
+            if (text.rangeOfCharacter(from: CharacterSet.alphanumerics) != nil || text.rangeOfCharacter(from: CharacterSet.whitespacesAndNewlines) != nil) {
+                if editableTextNode.attributedText?.string.count ?? 0 > self.maxTextLength {
+                    return false
+                }
+            }
+        }
+        return self.delegate?.editableTextNode(editableTextNode, shouldChangeTextIn: range, replacementText: text) ?? true
     }
 }
